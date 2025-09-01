e22d25c2a48f1d1e993ce9202b76c408d85662fd
Rander Torres <randertorres@qq.com>
2025-09-02 00:35:23 +0800

    Second assignment.

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,35 @@
-#include <printf.h>
+#include <stdio.h>
 
-int main() {
-    printf("hello world!\n");
+int is_prime(int n);
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (is_prime(n))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
     return 0;
+}
+
+int is_prime(int n)
+{
+    if (n==1)
+    {
+        return 0;
+    }
+    for (int i=2;i<=n/2;i++)    // 节省时间，只需算到二分之n
+    {
+        if (n%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
