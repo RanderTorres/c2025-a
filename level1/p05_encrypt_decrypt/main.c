d79e800f47bc3c77ed68f138f632e9bd1f661941
Rander Torres <randertorres@qq.com>
2025-09-03 20:47:34 +0800

    assignment.

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,48 @@
-#include <printf.h>
+// 凯撒加密
 
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+
+// 设置偏移量
+const int BIAS = -10;
+
+int encrypt(char *str);
+int decrypt(char *str);
+
+int main()
+{
+    char str[100];
+    printf("Input something to encrypt: ");
+    fgets(str, 100, stdin);
+
+    // 加密
+    encrypt(str);
+    printf("Encrypt: ");
+    fputs(str, stdout);
+
+    // 解密
+    decrypt(str);
+    printf("\nDecrypt: ");
+    fputs(str, stdout);
+
+
     return 0;
+
+}
+
+int encrypt(char *str)
+{
+    for (int i = 0; str[i] != '\n'; i++)
+    {
+        str[i] += BIAS;
+    }
+
+    return 0;
+}
+
+int decrypt(char *str)
+{
+    for (int i = 0; str[i] != '\n'; i++)
+    {
+        str[i] -= BIAS;
+    }
 }
