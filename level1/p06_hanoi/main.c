49afc578120812f8a044fa871c624b99494ee0d8
Rander Torres <randertorres@qq.com>
2025-09-29 20:26:35 +0800

    assignment.

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,30 @@
-#include <printf.h>
+#include <stdio.h>
 
-int main() {
-    printf("hello world!\n");
+void hanoi(int n, char fom, char by, char to);
+void moveOne(char from, char to);
+
+int main()
+{
+    hanoi(64, 'A', 'B', 'C');
     return 0;
+}
+
+void hanoi(int n, char fom, char by, char to)
+{
+    if (n==1)
+    {
+        moveOne(fom, to);
+        return ;
+    }
+    else
+    {
+        hanoi(n-1, fom, to, by);
+        moveOne(fom, to);
+        hanoi(n-1, by, fom, to);
+    }
+}
+
+void moveOne(char from, char to)
+{
+    printf("%c -> %c\n", from, to);
 }
